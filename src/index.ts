9dd6d874ed8a11a19771ee81a4fd896cb0b2a8fa
Toby Smith <tobysmith568@hotmail.co.uk>
2021-08-15 21:02:06 +0100

    Switch from JSON to TS

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Options }   from "prettier";
+
+const config: Options = {
+  printWidth: 100,
+  arrowParens: "avoid",
+  singleQuote: false,
+  trailingComma: "none",
+};
+
+export default config;
